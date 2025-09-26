e187fc10c8a6fd853e38c132d39d854a58b23d57
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-09-25 23:01:02 -0300

    Add Controller, Service, s3

---
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/Controller/UploadController.java
@@ -0,0 +1,22 @@
+package com.daniel.s3api.upload_api.Controller;
+
+import com.daniel.s3api.upload_api.Service.S3Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api")
+public class UploadController {
+    private final S3Service s3Service;
+
+    public UploadController(S3Service s3Service) {this.s3Service = s3Service;}
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file")MultipartFile file) throws Exception{
+        String bucketName = "teste-s3";
+        return s3Service.uploadFile(file, bucketName);
+    }
+}
---
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/Service/S3Service.java
@@ -0,0 +1,34 @@
+package com.daniel.s3api.upload_api.Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+    private final S3Client s3Client;
+
+    public S3Service(S3Client s3Client) {this.s3Client = s3Client;}
+
+    public String uploadFile(MultipartFile file, String bucketName) {
+        try {
+            String key = file.getOriginalFilename();
+
+            s3Client.putObject(
+                    PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(key)
+                            .build(),
+                    RequestBody.fromBytes(file.getBytes())
+            );
+
+            return "https://" + bucketName + ".s3.sa-east-1.amazonaws.com/" + key;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao enviar arquivo para S3", e);
+        }
+    }
+}
+
