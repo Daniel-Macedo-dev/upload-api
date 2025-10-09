9b4f8aa2ebde7d6086da57e9d74a9d9579f9abac
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-10-08 21:15:37 -0300

    Entities

--- upload-api/src/main/java/com/daniel/s3api/upload_api/infrastructure/entities/Print.java
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/infrastructure/entities/Print.java
@@ -1,5 +1,7 @@
 package com.daniel.s3api.upload_api.infrastructure.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -17,14 +19,46 @@ public class Print {
     private String description;
     private LocalDateTime uploadDate;
 
-    private
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private User user;
 
     public Print() {}
 
-
+    public Print(String filename, String game, String description, LocalDateTime uploadDate) {
+        this.filename = filename;
+        this.game = game;
+        this.description = description;
+        this.uploadDate = uploadDate;
+    }
 
     @PrePersist
     public void prePersist(){
         this.uploadDate = LocalDateTime.now();
     }
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getFilename() {return filename;}
+
+    public void setFilename(String filename) {this.filename = filename;}
+
+    public String getGame() {return game;}
+
+    public void setGame(String game) {this.game = game;}
+
+    public String getDescription() {return description;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public LocalDateTime getUploadDate() {return uploadDate;}
+
+    public void setUploadDate(LocalDateTime uploadDate) {this.uploadDate = uploadDate;}
+
+    public User getUser() {return user;}
+
+    public void setUser(User user) {this.user = user;}
 }
--- upload-api/src/main/java/com/daniel/s3api/upload_api/infrastructure/entities/User.java
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/infrastructure/entities/User.java
@@ -1,4 +1,60 @@
 package com.daniel.s3api.upload_api.infrastructure.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "user_table")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Print> prints = new ArrayList<>();
+
+
+    public User() {}
+
+    public User(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public User(String nome, String email, List<Print> prints) {
+        this.nome = nome;
+        this.email = email;
+        this.prints = prints;
+    }
+
+    public Integer getId() {return id;}
+
+    public void setId(Integer id) {this.id = id;}
+
+    public String getNome() {return nome;}
+
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public List<Print> getPrints() {return prints;}
+
+    public void setPrints(List<Print> prints) {this.prints = prints;}
+
+    public void addPrint(Print print){
+        prints.add(print);
+        print.setUser(this);
+    }
+    public void removePrint(Print print){
+        prints.remove(print);
+        print.setUser(null);
+    }
 }
