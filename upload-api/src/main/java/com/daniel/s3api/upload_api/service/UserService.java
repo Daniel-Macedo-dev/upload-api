4cc637b6388a91247ff7b0a201cf1bb04dc2958f
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-10-13 16:22:15 -0300

    Add UserService

---
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/service/UserService.java
@@ -0,0 +1,35 @@
+package com.daniel.s3api.upload_api.service;
+
+import com.daniel.s3api.upload_api.infrastructure.entities.User;
+import com.daniel.s3api.upload_api.infrastructure.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {this.userRepository = userRepository;}
+
+    public User salveUser(User user) {
+        return userRepository.saveAndFlush(user);
+    }
+    public List<User> listUsers (){
+        return userRepository.findAll();
+    }
+    public User searchUserById(Integer id){
+        return userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado"));
+    }
+    public User replaceUser (Integer id, User newUser){
+        User userSearch = userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado"));
+        userSearch.setNome(newUser.getNome());
+        userSearch.setEmail(newUser.getEmail());
+        return userRepository.saveAndFlush(userSearch);
+    }
+    public void deleteUser (Integer id){
+        userRepository.deleteById(id);
+    }
+}
