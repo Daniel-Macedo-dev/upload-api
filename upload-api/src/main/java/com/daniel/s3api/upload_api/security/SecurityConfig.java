842a6a824a9d3372f4e7fa41106ce80e0cbc9b31
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-10-15 02:55:22 -0300

    Adjustments

--- upload-api/src/main/java/com/daniel/s3api/upload_api/security/SecurityConfig.java
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                         .requestMatchers("/user", "/user/login").permitAll()
                         .anyRequest().authenticated()
                 )
+
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
