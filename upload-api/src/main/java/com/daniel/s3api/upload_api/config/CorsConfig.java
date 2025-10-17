5a5740550c34641764ae37febe8de18d41586673
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-10-17 18:19:36 -0300

    Add Cors with new config

--- upload-api/src/main/java/com/daniel/s3api/upload_api/config/CorsConfig.java
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/config/CorsConfig.java
@@ -7,13 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173"); // seu frontend
+                registry.addMapping("/**") // todas as rotas
+                        .allowedOrigins("http://localhost:5173") // frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
