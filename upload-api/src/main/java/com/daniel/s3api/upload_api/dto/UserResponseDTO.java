178b199cd8636ecc6d86fb19362f75377ae0cca1
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-11-18 17:10:38 -0300

    convert DTOs to records

--- upload-api/src/main/java/com/daniel/s3api/upload_api/dto/UserResponseDTO.java
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/dto/UserResponseDTO.java
@@ -2,21 +2,13 @@ package com.daniel.s3api.upload_api.dto;
 
 import com.daniel.s3api.upload_api.infrastructure.entities.User;
 
-public class UserResponseDTO {
-    private Integer id;
-    private String nome;
-    private String email;
-    private String role;
-
+public record UserResponseDTO(
+        Integer id,
+        String nome,
+        String email,
+        String role
+) {
     public UserResponseDTO(User user) {
-        this.id = user.getId();
-        this.nome = user.getNome();
-        this.email = user.getEmail();
-        this.role = user.getRole();
+        this(user.getId(), user.getNome(), user.getEmail(), user.getRole());
     }
-
-    public Integer getId() { return id; }
-    public String getNome() { return nome; }
-    public String getEmail() { return email; }
-    public String getRole() { return role; }
 }
