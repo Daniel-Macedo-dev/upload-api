23450f9b626fd28226cdf18fb358e3b430067532
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-05-12 20:05:59 -0300

    Configure S3 region from properties

--- upload-api/src/main/java/com/daniel/s3api/upload_api/s3/S3Config.java
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/s3/S3Config.java
@@ -1,5 +1,6 @@
 package com.daniel.s3api.upload_api.s3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -8,10 +9,16 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
+    private final String region;
+
+    public S3Config(@Value("${aws.s3.region}") String region) {
+        this.region = region;
+    }
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.SA_EAST_1)
+                .region(Region.of(region))
                 .build();
     }
 }
