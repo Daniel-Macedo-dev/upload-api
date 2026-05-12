72bc9f6b84c71347f7f30031c75a8051243573bb
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-05-12 20:07:31 -0300

    Read upload bucket name from configuration

--- upload-api/src/main/java/com/daniel/s3api/upload_api/controller/UploadController.java
+++ upload-api/src/main/java/com/daniel/s3api/upload_api/controller/UploadController.java
@@ -2,6 +2,7 @@ package com.daniel.s3api.upload_api.controller;
 
 import com.daniel.s3api.upload_api.service.S3Service;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +13,11 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController {
 
     private final S3Service s3Service;
+    private final String bucketName;
 
-    public UploadController(S3Service s3Service) {
+    public UploadController(S3Service s3Service, @Value("${aws.s3.bucket-name}") String bucketName) {
         this.s3Service = s3Service;
+        this.bucketName = bucketName;
     }
 
     @PostMapping("/upload")
@@ -25,7 +28,6 @@ public class UploadController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário não autenticado");
         }
 
-        String bucketName = "prints-jogos";
         String fileUrl = s3Service.uploadFile(file, bucketName);
         return ResponseEntity.status(HttpStatus.CREATED).body(fileUrl);
     }
