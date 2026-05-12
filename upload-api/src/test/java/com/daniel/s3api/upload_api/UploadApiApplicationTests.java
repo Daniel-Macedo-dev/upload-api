98587ba275efb90db0dd857151ff82f18520518a
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-05-12 20:13:41 -0300

    test: load test profile for application context

--- upload-api/src/test/java/com/daniel/s3api/upload_api/UploadApiApplicationTests.java
+++ upload-api/src/test/java/com/daniel/s3api/upload_api/UploadApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.daniel.s3api.upload_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UploadApiApplicationTests {
 
 	@Test
